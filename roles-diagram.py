7cca72df3e480bfcc9396007a0f69af9b4d8dfb5
Josh Chapman <joshuahchapman@users.noreply.github.com>
2021-04-03 21:30:57 -0700

    add option to pull roles from snowflake

--- roles-diagram.py
+++ roles-diagram.py
@@ -1,12 +1,40 @@
+from os import getenv
+import snowflake.connector
+import pandas as pd
 from diagrams import Diagram
 from diagrams.saas.analytics import Snowflake
-import pandas as pd
 
-# Read in Snowflake role relationships
-df = pd.read_csv('roles.csv')
 
+def get_roles_from_file():
+    df = pd.read_csv('roles.csv')
+    return df
+
+
+def get_roles_from_snowflake():
+    ctx = snowflake.connector.connect(
+        host=getenv("SNOWFLAKE_HOST"),
+        user=getenv("SNOWFLAKE_USER"),
+        password=getenv("SNOWFLAKE_PASSWORD"),
+        role="ACCOUNTADMIN",
+        account=getenv("SNOWFLAKE_ACCOUNT"),
+        warehouse=getenv("SNOWFLAKE_WAREHOUSE"),
+        database="SNOWFLAKE",
+        schema="ACCOUNT_USAGE",
+        protocol='https',
+        port=443)
+
+    cur = ctx.cursor()
+    sql = "select name as child_role, grantee_name as parent_role from snowflake.account_usage.grants_to_roles " \
+          "where granted_on = 'ROLE' and privilege = 'USAGE'"
+    cur.execute(sql)
+    df = cur.fetch_pandas_all()
+    return df
+
+
+roles_df = get_roles_from_file()
+# roles_df = get_roles_from_snowflake()
 hierarchy = {}
-for index, row in df.iterrows():
+for index, row in roles_df.iterrows():
     if row["PARENT_ROLE"] not in hierarchy:
         hierarchy[row["PARENT_ROLE"]] = [row["CHILD_ROLE"]]
     else:
